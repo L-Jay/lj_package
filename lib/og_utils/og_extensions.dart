0bf4dee17048e052fa3ecc30be4a6d4e2fdb0146
崔志伟 <xiaowei5486@126.com>
2021-04-21 09:35:08 +0800

    String扩展添加验证手机号方法

--- lib/og_utils/og_extensions.dart
+++ lib/og_utils/og_extensions.dart
@@ -9,6 +9,14 @@ extension StringExtensions on String {
   double toDouble() {
     return double.parse(this);
   }
+
+  bool verifyPhone() {
+    RegExp exp = RegExp(
+        r'^((13[0-9])|(14[0-9])|(15[0-9])|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\d{8}$');
+    bool matched = exp.hasMatch(this);
+
+    return matched;
+  }
 }
 
 extension StateExtension on State {
