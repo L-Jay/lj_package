ea3619e89544168f76694cf935fcbfc4882a4f35
radish <15703379121@163.com>
2021-05-28 17:43:45 +0800

    协议

---
+++ lib/og_ui_component/og_webview_page.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/platform_interface.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class OGWebViewPage extends StatefulWidget {
+  final String url;
+
+  OGWebViewPage(this.url);
+
+  @override
+  _OGWebViewPageState createState() => _OGWebViewPageState(url);
+}
+
+class _OGWebViewPageState extends State<OGWebViewPage> {
+  String _url;
+  String _title;
+  bool _canBack = false;
+  bool _canForward = false;
+
+  _OGWebViewPageState(this._url);
+
+  WebViewController _webViewController;
+
+  String toUtf8(String headHTML) {
+    return Uri.dataFromString(headHTML,
+            mimeType: 'text/html',
+            encoding: Encoding.getByName('utf-8'),
+            base64: true)
+        .toString();
+  }
+
+  Widget _webView() {
+    return WebView(
+      initialUrl: _url,
+      javascriptMode: JavascriptMode.unrestricted,
+      gestureNavigationEnabled: true,
+      onWebViewCreated: (controller) {
+        _webViewController = controller;
+      },
+      onPageStarted: (String url) {
+        // Utils.showLoading();
+        print('开始加载$url');
+      },
+      onPageFinished: (String url) async {
+        // Utils.dismissLoading();
+        print('完成加载$url');
+        _title = await _webViewController.getTitle();
+        _canBack = await _webViewController.canGoBack();
+        _canForward = await _webViewController.canGoForward();
+
+        setState(() {});
+      },
+      onWebResourceError: (WebResourceError error) {
+        // Utils.dismissLoading();
+        print('加载失败' + error.failingUrl + error.description);
+      },
+      navigationDelegate: (NavigationRequest request) async {
+        return NavigationDecision.navigate;
+      },
+      javascriptChannels: <JavascriptChannel>[].toSet(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: CloseButton(),
+        title: Text(_title ?? ''),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: _webView(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
